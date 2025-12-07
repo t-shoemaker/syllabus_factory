f6851275d2e6a30ee9668541a6d6057c2d492d40
Tyler Shoemaker <69219954+t-shoemaker@users.noreply.github.com>
2025-12-07 12:57:41 -0600

    Use separate functions for section formatting

--- src/compile.py
+++ src/compile.py
@@ -4,6 +4,60 @@ from templates import MarkdownEntry, SpecialCourseDesignation
 from utils import wrap_paragraphs, flatten_config
 
 
+def format_toml_tables(syllabus_data, items=None):
+    """Format tables of arrays.
+
+    Parameters
+    ----------
+    syllabus_data : dict
+        Course metadata
+    items : iterable or None
+        Keys to access tables
+
+    Returns
+    -------
+    dict
+        Syllabus data with formatted tables
+    """
+    for item in items:
+        entries = []
+        for entry in syllabus_data[item]:
+            entry_md = MarkdownEntry[item.upper()].render(**entry)
+            entries.append(wrap_paragraphs(entry_md))
+
+        syllabus_data[item + "s"] = "\n".join(entries)
+
+    return syllabus_data
+
+
+def format_schedule(schedule):
+    """Format the schedule.
+
+    Parameters
+    ----------
+    schedule : dict
+        The schedule weeks
+
+    Returns
+    -------
+    str
+        Formatted schedule
+    """
+    schedule_md = []
+    for week in schedule.values():
+        num = week.get("num", "")
+        title = week.get("title", "")
+
+        week_md = MarkdownEntry.WEEK.render(num=num, title=title)
+        schedule_md.append(week_md)
+
+        for day in week.get("days", []):
+            day_md = format_day(day)
+            schedule_md.append(day_md)
+
+    return "\n".join(schedule_md)
+
+
 def format_day(day):
     """Format a day.
 
@@ -40,6 +94,32 @@ def format_day(day):
     )
 
 
+def format_course_designation(designation):
+    """Format course designation.
+
+    Parameters
+    ----------
+    designation : str
+        The course designation code
+
+    Returns
+    -------
+    str
+        Course designation
+
+    Raises
+    ------
+    NotImplementedError
+        If the course designation doesn't have a template
+    """
+    if designation == "None":
+        return designation
+    elif designation == "KLPC":
+        return SpecialCourseDesignation.KLPC.render()
+    else:
+        raise NotImplementedError(f"No template for {designation}")
+
+
 def compile_md(syllabus_data, schedule, md_files):
     """Compile Markdown and send to stdout.
 
@@ -52,46 +132,18 @@ def compile_md(syllabus_data, schedule, md_files):
     md_files : list[Path]
         Markdown template files for each syllabus component
     """
-    # Format top-level information
-    for item in ("assignment", "book", "objective"):
-        entries = []
-        for entry in syllabus_data[item]:
-            md = MarkdownEntry[item.upper()].render(**entry)
-            entries.append(wrap_paragraphs(md))
+    items = ("assignment", "book", "objective")
+    syllabus_data = format_toml_tables(syllabus_data, items)
+    syllabus_data["course_schedule"] = format_schedule(schedule)
 
-        syllabus_data[item + "s"] = "\n".join(entries)
-
-    # Format the schedule
-    schedule_md = []
-    for week in schedule.values():
-        md = MarkdownEntry.WEEK.render(
-            num=week.get("num", ""),
-            title=week.get("title", ""),
-        )
-        schedule_md.append(md)
-        for day in week.get("days", []):
-            md = format_day(day)
-            schedule_md.append(md)
-
-    syllabus_data["course_schedule"] = "\n".join(schedule_md)
-
-    # Flatten the schedule config and wrap the course description
+    # Flatten the config for the rest of the items -- they aren't nested
     syllabus_data = flatten_config(syllabus_data)
-    if "course_description" in syllabus_data:
-        syllabus_data["course_description"] = wrap_paragraphs(
-            syllabus_data["course_description"], width=79
-        )
+    syllabus_data["course_description"] = wrap_paragraphs(
+        syllabus_data.get("course_description", ""), width=79
+    )
 
-    # Format course designation
-    designation = syllabus_data.get("course_designation", "None")
-    if designation != "None":
-        match designation:
-            case "KLPC":
-                syllabus_data["course_designation"] = (
-                    SpecialCourseDesignation.KLPC.render()
-                )
-            case _:
-                raise ValueError(f"No designation for {designation}")
+    desig = syllabus_data.get("course_designation", "None")
+    syllabus_data["course_designation"] = format_course_designation(desig)
 
     # Open the markdown files, then unpack syllabus values in the mapping and
     # stream out the results
